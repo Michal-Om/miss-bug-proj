bc3522effc4828c992f7728bcbf4e1fd430ca544
Michal-Om <michal_omri@yahoo.com>
2025-08-08 18:32:21 +0300

    Add user.json and userService for user management

--- server.js
+++ server.js
@@ -120,6 +120,15 @@ app.delete('/api/bug/:bugId', (req, res) => {
         })
 })
 
+//User API
+app.get('/api/user', (req, res) => {
+    userService.query()
+        .then(users => res.send(users))
+        .catch(err => {
+            loggerService.error('Cannot load users', err)
+            res.status(400).send('Cannot load users')
+        })
+})
 
 const port = 3030
 app.listen(port, () => loggerService.info(`Server listening on port http://127.0.0.1:${port}/`))
